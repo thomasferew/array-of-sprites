815bd0697ca9b4cad9f6c688ffb607ac903c257e
thomasferew <58610700+thomasferew@users.noreply.github.com>
2020-02-28 13:29:18 -0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,18 +7,18 @@ let list: Sprite[] = []
 let favoritefood1 = sprites.create(img`
 . . . . . . . e c 7 . . . . . . 
 . . . . e e e c 7 7 e e . . . . 
-. . c e e e e c 7 e 2 2 e e . . 
-. c e e e e e c 6 e e 2 2 2 e . 
-. c e e e 2 e c c 2 4 5 4 2 e . 
-c e e e 2 2 2 2 2 2 4 5 5 2 2 e 
-c e e 2 2 2 2 2 2 2 2 4 4 2 2 e 
-c e e 2 2 2 2 2 2 2 2 2 2 2 2 e 
-c e e 2 2 2 2 2 2 2 2 2 2 2 2 e 
-c e e 2 2 2 2 2 2 2 2 2 2 2 2 e 
-c e e 2 2 2 2 2 2 2 2 2 2 4 2 e 
-. e e e 2 2 2 2 2 2 2 2 2 4 e . 
-. 2 e e 2 2 2 2 2 2 2 2 4 2 e . 
-. . 2 e e 2 2 2 2 2 4 4 2 e . . 
+. . c e e e e c 7 e 2 3 e e . . 
+. c e e e e e c 6 e e 2 3 2 e . 
+. c e e e 3 e c c 2 4 5 4 3 e . 
+c e e e 3 2 3 2 3 2 4 5 5 2 3 e 
+c e e 3 2 3 2 3 2 3 2 4 4 3 2 e 
+c e e 2 3 2 3 2 3 2 3 2 3 2 3 e 
+c e e 3 2 3 2 3 2 3 2 3 2 3 2 e 
+c e e 2 3 2 3 2 3 2 3 2 3 2 3 e 
+c e e 3 2 3 2 3 2 3 2 3 2 4 2 e 
+. e e e 3 2 3 2 3 2 3 2 3 4 e . 
+. 2 e e 2 3 2 3 2 3 2 3 4 2 e . 
+. . 2 e e 2 3 2 3 2 4 4 2 e . . 
 . . . 2 2 e e 4 4 4 2 e e . . . 
 . . . . . 2 2 e e e e . . . . . 
 `, SpriteKind.Player)
@@ -27,17 +27,17 @@ let favoritefood2 = sprites.create(img`
 . . . . . . . . . 6 6 7 7 7 7 8 
 . . . . . . 8 8 8 7 7 8 8 6 8 8 
 . . e e e e c 6 6 8 8 . 8 7 8 . 
-. e 2 5 4 2 e c 8 . . . 6 7 8 . 
-e 2 4 2 2 2 2 2 c . . . 6 7 8 . 
-e 2 2 2 2 2 2 2 c . . . 8 6 8 . 
-e 2 e e 2 2 2 2 e e e e c 6 8 . 
-c 2 e e 2 2 2 2 e 2 5 4 2 c 8 . 
-. c 2 e e e 2 e 2 4 2 2 2 2 c . 
-. . c 2 2 2 e e 2 2 2 2 2 2 2 e 
-. . . e c c e c 2 2 2 2 2 2 2 e 
-. . . . . . . c 2 e e 2 2 e 2 c 
+. e 7 5 4 7 e c 8 . . . 6 7 8 . 
+e 2 4 2 7 2 7 2 c . . . 6 7 8 . 
+e 7 2 7 2 7 2 7 c . . . 8 6 8 . 
+e 2 e e 7 2 7 2 e e e e c 6 8 . 
+c 7 e e 2 7 2 7 e 7 5 4 7 c 8 . 
+. c 2 e e e 7 e 2 4 2 7 2 7 c . 
+. . c 7 2 7 e e 7 2 7 2 7 2 7 e 
+. . . e c c e c 2 7 2 7 2 7 2 e 
+. . . . . . . c 7 e e 2 7 e 7 c 
 . . . . . . . c e e e e e e 2 c 
-. . . . . . . . c e 2 2 2 2 c . 
+. . . . . . . . c e 2 7 2 7 c . 
 . . . . . . . . . c c c c c . . 
 `, SpriteKind.Player)
 list = [sprites.create(img`
